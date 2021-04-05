0125c73705b84b8132b1c0d7efcbb76f6fc97fe3
OM Miles <olivia.m.miles@gmail.com>
2021-04-05 14:27:52 -0700

    Lab completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Declare num variables as const so they cannot be re-assigned or re-declared
+const num1 = 4, num2 = 15.5, num3 = 160, num4 = 40
 
-// Write your code here
+/* Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62. */
+let multiply = (num1 * num2)
+
+/* Create a variable called random that will generate a random integer greater than 0. */
+let random = Number.isInteger(Math.random())+1
+
+/* Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4. */
+let mod = (num3 / num4)
+
+/* Create a variable called max that finds the highest number in a set; the value returned should be 20. */
+let max = Math.max(20, 19, 18, 17, -1)
